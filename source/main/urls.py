ae822a7162cd2fde696f81da8d5c93de505c7f9b
galubezzz galubezzz <galubezzzgalubezzz@galubezzzs-MacBook-Air.local>
2018-12-20 02:50:46 -0800

    added form for post create and edit

--- source/main/urls.py
+++ source/main/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from webapp.views import UserListView, UserDetailView, PostDetailView, PostListView, FavoritesView
+from webapp.views import UserListView, UserDetailView, PostDetailView, PostListView, FavoritesView, PostCreateView, PostUpdateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +24,6 @@ urlpatterns = [
     path('', PostListView.as_view(), name='posts_list'),
     path('posts/<int:pk>', PostDetailView.as_view(), name='post_detail'),
     path('favorites/<int:pk>', FavoritesView.as_view(), name='favorites'),
+    path('posts/create', PostCreateView.as_view(), name='post_create'),
+    path('posts/<int:pk>/update', PostUpdateView.as_view(), name='post_update'),
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from webapp.models import User, Post, Comment, Rating
+from webapp.forms import PostForm
+from django.urls import reverse, reverse_lazy
 
 
 class UserListView(ListView):
@@ -27,3 +29,17 @@ class PostDetailView(DetailView):
 class FavoritesView(DetailView):
     model = User
     template_name = 'favorites.html'
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'post_create.html'
+    form_class = PostForm
+    success_url = reverse_lazy('posts_list')
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_update.html'
+    form_class = PostForm
+    success_url = reverse_lazy('posts_list')
+
