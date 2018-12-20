26a3c743057a0ae6b2a49f3ba97d497e8bb4b9e9
galubezzz galubezzz <galubezzzgalubezzz@galubezzzs-MacBook-Air.local>
2018-12-19 23:35:06 -0800

    added models and registered them in admin

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from webapp.models import User, Post, Comment, Rating
+
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Rating)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+
+class User(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Имя пользователя")
+    email = models.CharField(max_length=50, verbose_name="E-mail")
+    favorites = models.ManyToManyField('Post', blank=True, related_name='favored_by', verbose_name='Избранное')
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=50, verbose_name="Заголовок")
+    text = models.TextField(max_length=5000, verbose_name="Текст")
+    date = models.DateTimeField(default=datetime.now, verbose_name='Дата')
+    author = models.ForeignKey(User, on_delete=models.PROTECT, related_name='posts_by_user', verbose_name='Автор')
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    text = models.TextField(max_length=1000, verbose_name="Комментарий")
+    date = models.DateTimeField(default=datetime.now, verbose_name='Дата')
+    author = models.ForeignKey(User, on_delete=models.PROTECT, related_name='comments', verbose_name='Автор')
+    parent_comment = models.ForeignKey('Comment', null=True, blank=True, on_delete=models.PROTECT, related_name='child_comments', verbose_name='Родительский комментарий')
+    date = models.DateTimeField(default=datetime.now, verbose_name='Дата')
+    post = models.ForeignKey(Post, on_delete=models.PROTECT, related_name='comments', verbose_name='Пост')
+
+    def __str__(self):
+        return self.text
+
+
+class Rating(models.Model):
+    RATING_1 = 'awful'
+    RATING_2 = 'bad'
+    RATING_3 = 'average'
+    RATING_4 = 'well'
+    RATING_5 = 'excellent'
+
+    RATING_CHOICES = (
+        (RATING_1, 'Посредственно'),
+        (RATING_2, 'Неудовлетворительно'),
+        (RATING_3, 'Удовлетворительно'),
+        (RATING_4, 'Хорошо'),
+        (RATING_5, 'Отлично')
+    )
+
+    post = models.ForeignKey(Post, on_delete=models.PROTECT, related_name='ratings', verbose_name='Пост')
+    user = models.ForeignKey(User, on_delete=models.PROTECT, related_name='ratings', verbose_name='Пользователь')
+    value = models.CharField(max_length=20, choices=RATING_CHOICES, verbose_name='Оценка')
+
+    def __str__(self):
+        return self.get_value_display()
