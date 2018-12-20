535d77da63a57198f5a3e51ed91121f98002be4a
galubezzz galubezzz <galubezzzgalubezzz@galubezzzs-MacBook-Air.local>
2018-12-20 03:06:48 -0800

    added form for comment create and edit

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -9,7 +9,7 @@ class PostForm(forms.ModelForm):
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ['parent_comment', 'text', 'article', 'author']
+        fields = ['parent_comment', 'text', 'post', 'author']
 
 
 class CommentUpdateForm(forms.ModelForm):
