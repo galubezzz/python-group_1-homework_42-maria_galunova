ff642cadeba04e1ca4d8961aedb903e958621109
galubezzz galubezzz <galubezzzgalubezzz@galubezzzs-MacBook-Air.local>
2018-12-20 00:54:18 -0800

    added views and templates for User

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView, DetailView
+from webapp.models import User, Post, Comment, Rating
+
+
+class UserListView(ListView):
+    model = User
+    template_name = 'user_list.html'
+
+
+class UserDetailView(DetailView):
+    model = User
+    template_name = 'user_details.html'
+
+
