04bb1fb43048f388ef16280ec1530fce713764ae
galubezzz galubezzz <galubezzzgalubezzz@galubezzzs-MacBook-Air.local>
2018-12-20 00:56:01 -0800

    updated urls

--- source/main/urls.py
+++ source/main/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from webapp.views import UserListView, UserDetailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users', UserListView.as_view(), name='user_list'),
+    path('users/<int:pk>', UserDetailView.as_view(), name='user_detail'),
 ]
